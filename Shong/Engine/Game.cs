3edd4b36fda5759733d3aa5e00c928504b2f5898
Chris Font <cfont3@uic.edu>
2015-05-20 23:54:54 -0500

    Logging system added
    System supports printing to console (LogConsole)and saving as a file
    (LogMsg)

--- Shong/Engine/Game.cs
+++ Shong/Engine/Game.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shong.Engine;
 
 namespace Shong
 {
     class Game
     {
+        public Game()
+        {
+        }
+
+        public void Run()
+        {
+            // Initialize Logging System
+            Log.Instance.FilePath = "../../shong.log";
+
+            // Initialize Settings
+
+            // Clean Up
+        }
     }
 }
---
+++ Shong/Engine/Log.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Shong.Engine
+{
+    enum LogType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    class Log
+    {
+        private static          Log    _instance  = null;
+        private static readonly object Padlock    = new object();
+        public bool                    ConsoleOut = true;
+        public string                  FilePath   = null;
+
+        Log()
+        {
+        }
+
+        public static Log Instance
+        {
+            get
+            {
+                lock (Padlock)
+                {
+                    return _instance ?? (_instance = new Log());
+                }
+            }
+        }
+
+        void LogMsg(LogType type, string logMsg)
+        {
+            if (FilePath != null)
+            {
+                using (var stream = new StreamWriter(FilePath, true))
+                {
+                    stream.WriteLine("{0}\t{1}\t{2}", type, DateTime.Now, logMsg);
+                }
+            }
+            else
+            {
+                // Attempting to use the log before log file is set
+                LogCon(LogType.Error, "Log file not set!");
+            }
+
+            if (ConsoleOut) LogCon(type, logMsg);
+        }
+
+        void LogCon(LogType type, string logMsg)
+        {
+            Console.WriteLine("{0}\t{1}\t{2}", 
+                type,
+                DateTime.Now,
+                logMsg);
+        }
+    }
+}
